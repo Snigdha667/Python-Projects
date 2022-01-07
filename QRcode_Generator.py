3247caa3d2bd9b3bb927ce0c99eb6e538b6f002e
Snigdha Singh <90649383+Snigdha667@users.noreply.github.com>
2022-01-07 23:24:59 +0530

    Update QRcode_Generator.py

--- QRcode_Generator.py
+++ QRcode_Generator.py
@@ -1,5 +1,5 @@
 import qrcode
-img = qrcode.make("https://www.github.com/Snigdha667/Python-Projects")
+img = qrcode.make("https://www.github.com/Snigdha667/Python-Projects/commit/0864a394932049e7b0bb6dc5c04d20ab8f99ce10")
 img.save("My_Github_Account.jpg")
 import cv2
 d= cv2.QRCodeDetector()
